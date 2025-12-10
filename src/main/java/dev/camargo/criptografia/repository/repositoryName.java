1931c96a33db7396954fca31fbb4bb5cc39a2360
Matheus Camargo <matheuscamargotech@gmail.com>
2025-12-10 00:50:36 -0300

    feat: UserRepository interface
    Extends JpaRepository to provide CRUD operations for User entity.
    Allows data persistence without raw SQL implementation.

---
+++ src/main/java/dev/camargo/criptografia/repository/repositoryName.java
@@ -0,0 +1,7 @@
+package dev.camargo.criptografia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface repositoryName extends JpaRepository<String, String>{}
---
+++ src/main/java/dev/camargo/criptografia/service/UserService.java
@@ -0,0 +1,5 @@
+package dev.camargo.criptografia.service;
+
+public class UserService {
+    
+}
